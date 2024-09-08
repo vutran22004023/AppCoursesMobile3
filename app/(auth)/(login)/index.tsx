f934fb036e7f97e24c4297029bd91dd9d5ddedbd
vutran22004023 <levu1962004@gmail.com>
2024-09-08 16:13:47 +0700

    update ui login register new

--- app/(auth)/(login)/index.tsx
+++ app/(auth)/(login)/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView, View, Image, Text, Alert, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import FormField from '@/components/Common/FormField/formField';
 import { images } from '@/constants';
 import ButtonComponent from '@/components/Common/Button/button';
@@ -15,8 +16,9 @@ import TextThemed from '@/components/Common/TextThemed';
 // import useNavigation from '@/hooks/useNavigation';
 import { ThemedView } from '@/components/Common/ViewThemed';
 import { initializeUser } from '@/contexts/private';
-import { useRouter,Link } from 'expo-router';
+import { useRouter, Link } from 'expo-router';
 import useToast from '@/hooks/useToast';
+import Animated, { FadeIn, FadeInDown, FadeInUp, FadeOut } from 'react-native-reanimated';
 
 type DataLogin = {
   status?: any;
@@ -25,7 +27,7 @@ type DataLogin = {
   id?: string;
 };
 const index = () => {
-  const { Toast,showToast } = useToast();
+  const { Toast, showToast } = useToast();
   const dispatch = useDispatch<AppDispatch>();
   const navigation = useRouter();
   const [valueLogin, setValueLogin] = useState({
@@ -60,10 +62,10 @@ const index = () => {
             access_Token: (dataLogin as DataLogin).access_Token,
           })
         );
-        showToast({ type: 'success', text: 'Bạn đã đăng nhập thành công' })
+        showToast({ type: 'success', text: 'Bạn đã đăng nhập thành công' });
         await initializeUser(dispatch, navigation);
-      }else {
-        showToast({ type: 'error', text: 'Bạn đã đăng nhập thất bại' })
+      } else {
+        showToast({ type: 'error', text: 'Bạn đã đăng nhập thất bại' });
       }
     };
     login();
@@ -77,58 +79,82 @@ const index = () => {
   }, [dispatch, navigation]);
   const submit = () => {
     if (!valueLogin.email || !valueLogin.password) {
-      showToast({ type: 'warning', text: 'Vui lòng điền đầy đủ thông tin' })
+      showToast({ type: 'warning', text: 'Vui lòng điền đầy đủ thông tin' });
       return;
     }
     mutationLogin.mutate(valueLogin);
   };
   return (
     <ThemedView>
+      <StatusBar style="light" />
       <ScrollView>
-        <View className="my-6 min-h-[85vh] w-full justify-center px-4">
-          <Image source={images.logo} resizeMode="contain" className="h-[84px] w-[130px]" />
-          <TextThemed type="title"> Đăng nhập</TextThemed>
-
-          <FormField
-            title="Email"
-            value={valueLogin.email}
-            name="email"
-            handleChangeText={(text) => handleOnchange(text, 'email')}
-            otherStyles={`mt-7`}
-            keyboardType="email-address"
-            placeholder=""
-          />
-
-          <FormField
-            title="Mật khẩu"
-            name="password"
-            value={valueLogin.password}
-            handleChangeText={(text) => handleOnchange(text, 'password')}
-            otherStyles={`mt-7`}
-            keyboardType="default"
-            placeholder=""
-          />
-
-          <ButtonComponent
-            title="Đăng nhập"
-            handlePress={submit}
-            containerStyles={`mt-7`}
-            isLoading={isLoading}
-          />
-
-          <View className="flex-row justify-center gap-2 pt-5">
-            <TextThemed className="text-lg text-gray-100">Bạn chưa có tài khoản ?</TextThemed>
-            <TouchableOpacity onPress={() => navigation.push('/(auth)/(register)')}>
-              <Text className="text-secondary text-lg font-bold font-pregular">
-                Đăng kí
-              </Text>
-            </TouchableOpacity>
-          </View>
+      <Image
+        className="absolute h-[400] w-full"
+        source={require('@/assets/images/Background.png')}
+      />
+      <View className="absolute h-[225] w-full flex-row justify-around">
+        <Animated.Image
+          entering={FadeInUp.delay(200).duration(1000).springify().damping(3)}
+          className="h-[225] w-[90]"
+          source={require('@/assets/images/Light.png')}
+        />
+        <Animated.Image
+          entering={FadeInUp.delay(400).duration(1000).springify().damping(3)}
+          className="h-[160] w-[65]"
+          source={require('@/assets/images/Light.png')}
+        />
+      </View>
+      <View className='w-full' style={{ marginTop: 240 }}>
+        <View className="w-full items-center justify-center">
+          <Animated.Text
+            entering={FadeInUp.duration(1000).springify()}
+            className="text-4xl font-bold tracking-wider text-white">
+            Đăng nhập
+          </Animated.Text>
         </View>
+        <View className="my-6 min-h-[60vh] w-full justify-center px-4">
+          <Animated.View entering={FadeInDown.duration(1000).springify()}>
+            <FormField
+              title="Email"
+              value={valueLogin.email}
+              name="email"
+              handleChangeText={(text) => handleOnchange(text, 'email')}
+              otherStyles={`mt-7`}
+              keyboardType="email-address"
+              placeholder=""
+            />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.duration(1000).springify()}>
+            <FormField
+              title="Mật khẩu"
+              name="password"
+              value={valueLogin.password}
+              handleChangeText={(text) => handleOnchange(text, 'password')}
+              otherStyles={`mt-7`}
+              keyboardType="default"
+              placeholder=""
+            />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.duration(1000).springify()}>
+            <ButtonComponent
+              title="Đăng nhập"
+              handlePress={submit}
+              containerStyles={`mt-7`}
+              isLoading={isLoading}
+            />
+          </Animated.View>
+          <Animated.View  entering={FadeInDown.duration(1500).springify()} className="flex-row justify-center gap-2 pt-5">
+            <TextThemed type="subtitle" className="text-lg text-gray-100">Bạn chưa có tài khoản ?</TextThemed>
+            <TouchableOpacity onPress={() => navigation.push('/(auth)/(register)')}>
+              <Text className="font-pregular text-lg font-bold text-secondary">Đăng kí</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+      </View>
       </ScrollView>
-        {Toast}
+      {Toast}
     </ThemedView>
-  )
-}
+  );
+};
 
-export default index
+export default index;
--- app/(auth)/(register)/index.tsx
+++ app/(auth)/(register)/index.tsx
@@ -11,8 +11,10 @@ import { ThemedView } from '@/components/Common/ViewThemed';
 import { useRouter } from 'expo-router';
 import TextThemed from '@/components/Common/TextThemed';
 import useToast from '@/hooks/useToast';
+import Animated, { FadeIn, FadeInDown, FadeInUp, FadeOut } from 'react-native-reanimated';
+import { StatusBar } from 'expo-status-bar';
 const index = () => {
-  const { Toast,showToast } = useToast();
+  const { Toast, showToast } = useToast();
   const navigation = useRouter();
   const [valueRegister, setValueRegister] = useState({
     name: '',
@@ -70,97 +72,124 @@ const index = () => {
       !valueRegister.password ||
       !valueRegister.confirmPassword
     ) {
-      showToast({ type: 'warning', text: 'Vui lòng điền đầy đủ thông tin' })
+      showToast({ type: 'warning', text: 'Vui lòng điền đầy đủ thông tin' });
       return;
     }
 
     if (!isValidEmail(valueRegister.email)) {
-      showToast({ type: 'warning', text: 'Email không hợp lệ'})
+      showToast({ type: 'warning', text: 'Email không hợp lệ' });
       return;
     }
 
     if (!isValidPassword(valueRegister.password)) {
-      showToast({ type: 'warning', text: 'Mật khẩu không hợp lệ. Mật khẩu phải có ít nhất 6 ký tự, bao gồm ít nhất một ký tự đặc biệt và một ký tự viết hoa.'})
+      showToast({
+        type: 'warning',
+        text: 'Mật khẩu không hợp lệ. Mật khẩu phải có ít nhất 6 ký tự, bao gồm ít nhất một ký tự đặc biệt và một ký tự viết hoa.',
+      });
       return;
     }
 
     if (!passwordsMatch(valueRegister.password, valueRegister.confirmPassword)) {
-      showToast({ type: 'warning', text: 'Mật khẩu và xác nhận mật khẩu không khớp.'})
+      showToast({ type: 'warning', text: 'Mật khẩu và xác nhận mật khẩu không khớp.' });
       return;
     }
     mutationRegister.mutate(valueRegister);
   };
   return (
     <ThemedView>
-    <ScrollView>
-      <View className="my-6 min-h-[85vh] w-full justify-center px-4">
-        <Image source={images.logo} resizeMode="contain" className="h-[84px] w-[130px]" />
-        <Text className="text-2xl font-semibold text-white"> Đăng nhập</Text>
-
-        <FormField
-          title="Họ và tên"
-          value={valueRegister.name}
-          name="name"
-          handleChangeText={(text) => handleOnchange(text, 'name')}
-          otherStyles={`mt-7`}
-          keyboardType="name-address"
-          placeholder=""
+      <StatusBar style="light" />
+      <ScrollView>
+        <Image
+          className="absolute h-[300] w-full"
+          source={require('@/assets/images/Background.png')}
         />
-
-        <FormField
-          title="Email"
-          value={valueRegister.email}
-          name="email"
-          handleChangeText={(text) => handleOnchange(text, 'email')}
-          otherStyles={`mt-7`}
-          keyboardType="email-address"
-          placeholder=""
-        />
-
-        <FormField
-          title="Mật khẩu"
-          name="password"
-          value={valueRegister.password}
-          handleChangeText={(text) => handleOnchange(text, 'password')}
-          otherStyles={`mt-7`}
-          keyboardType="password-address"
-          placeholder=""
-        />
-        <FormField
-          title="Xác nhận mật khẩu"
-          name="confirmPassword"
-          value={valueRegister.confirmPassword}
-          handleChangeText={(text) => handleOnchange(text, 'confirmPassword')}
-          otherStyles={`mt-7`}
-          keyboardType="confirmPassword-address"
-          placeholder=""
-        />
-
-        {dataRegister?.status === 'ERR' && (
-          <Text className="mt-3 text-base text-[#aa232a]">{dataRegister?.message}</Text>
-        )}
-        {dataRegister?.status === 200 && (
-          <Text className="mt-3 text-base text-[#3df033]">{dataRegister?.message}</Text>
-        )}
-
-        <ButtonComponent
-          title="Đăng kí"
-          handlePress={submit}
-          containerStyles={`mt-7`}
-          isLoading={isLoading}
-        />
-
-        <View className="flex-row justify-center gap-2 pt-5">
-          <TextThemed className="text-lg text-gray-100">Bạn đã có tài khoản ?</TextThemed>
-          <TouchableOpacity onPress={() => navigation.push('/(auth)/(login)')}>
-            <Text className="text-lg font-semibold text-secondary">Đăng nhập</Text>
-          </TouchableOpacity>
+        <View className="absolute  w-full flex-row justify-around">
+          <Animated.Image
+            entering={FadeInUp.delay(200).duration(1000).springify().damping(3)}
+            className="h-[225] w-[90]"
+            source={require('@/assets/images/Light.png')}
+          />
+          <Animated.Image
+            entering={FadeInUp.delay(400).duration(1000).springify().damping(3)}
+            className="h-[160] w-[65]"
+            source={require('@/assets/images/Light.png')}
+          />
         </View>
-      </View>
-    </ScrollView>
-    {Toast}
-  </ThemedView>
-  )
-}
+        <View className="w-full" style={{ marginTop: 320 }}>
+          <View className="w-full items-center justify-center">
+            <Animated.Text
+              entering={FadeInUp.duration(1000).springify()}
+              className="text-4xl font-bold tracking-wider text-white">
+              Đăng ký
+            </Animated.Text>
+          </View>
+          <View className="my-6 min-h-[60vh] w-full justify-center px-4">
+            <Animated.View entering={FadeInDown.duration(1000).springify()}>
+              <FormField
+                title="Họ và tên"
+                value={valueRegister.name}
+                name="name"
+                handleChangeText={(text) => handleOnchange(text, 'name')}
+                otherStyles={`mt-7`}
+                keyboardType="name-address"
+                placeholder=""
+              />
+            </Animated.View>
+            <Animated.View entering={FadeInDown.duration(1000).springify()}>
+              <FormField
+                title="Email"
+                value={valueRegister.email}
+                name="email"
+                handleChangeText={(text) => handleOnchange(text, 'email')}
+                otherStyles={`mt-7`}
+                keyboardType="email-address"
+                placeholder=""
+              />
+            </Animated.View>
+            <Animated.View entering={FadeInDown.duration(1000).springify()}>
+              <FormField
+                title="Mật khẩu"
+                name="password"
+                value={valueRegister.password}
+                handleChangeText={(text) => handleOnchange(text, 'password')}
+                otherStyles={`mt-7`}
+                keyboardType="password-address"
+                placeholder=""
+              />
+            </Animated.View>
+            <Animated.View entering={FadeInDown.duration(1000).springify()}>
+              <FormField
+                title="Xác nhận mật khẩu"
+                name="confirmPassword"
+                value={valueRegister.confirmPassword}
+                handleChangeText={(text) => handleOnchange(text, 'confirmPassword')}
+                otherStyles={`mt-7`}
+                keyboardType="confirmPassword-address"
+                placeholder=""
+              />
+            </Animated.View>
+            <Animated.View entering={FadeInDown.duration(1000).springify()}>
+              <ButtonComponent
+                title="Đăng kí"
+                handlePress={submit}
+                containerStyles={`mt-7`}
+                isLoading={isLoading}
+              />
+            </Animated.View>
+            <Animated.View
+              entering={FadeInDown.duration(1500).springify()}
+              className="flex-row justify-center gap-2 pt-5">
+              <TextThemed type="subtitle">Bạn đã có tài khoản ?</TextThemed>
+              <TouchableOpacity onPress={() => navigation.push('/(auth)/(login)')}>
+                <Text className="text-lg font-semibold text-secondary">Đăng nhập</Text>
+              </TouchableOpacity>
+            </Animated.View>
+          </View>
+        </View>
+      </ScrollView>
+      {Toast}
+    </ThemedView>
+  );
+};
 
-export default index
+export default index;
